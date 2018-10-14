c734d0d6232452cb9f469540cc23f297006313fa
Todd Rosner <toddrosner@gmail.com>
2018-10-13 20:18:10 -0700

    updates to node pools

--- deployments/development/us-west2/compute/kubernetes-engine/fabric-cluster/main.tf
+++ deployments/development/us-west2/compute/kubernetes-engine/fabric-cluster/main.tf
@@ -1,15 +1,25 @@
 resource "google_container_cluster" "primary" {
-  name               = "${var.cluster_name}"
-  network            = "${var.network}"
-  zone               = "${var.zone}"
-  initial_node_count = "${var.initial_node_count}"
+  name                     = "${var.cluster_name}"
+  network                  = "${var.network}"
+  zone                     = "${var.zone}"
+  additional_zones         = "${var.additional_zones}"
+  remove_default_node_pool = true
 
-  additional_zones = "${var.additional_zones}"
+  node_pool {
+    name = "default-pool"
+  }
 
   master_auth {
     username = "${var.master_auth_username}"
     password = "${var.master_auth_password}"
   }
+}
+
+resource "google_container_node_pool" "endorsers" {
+  name       = "endorsers"
+  zone       = "${var.zone}"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = "${var.node_count}"
 
   node_config {
     oauth_scopes = [
@@ -20,29 +30,51 @@ resource "google_container_cluster" "primary" {
     ]
 
     labels {
-      foo     = "bar"
-      service = "test"
+      service = "endorsing"
     }
 
     tags = "${var.tags}"
   }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
 }
 
-resource "google_container_node_pool" "primary" {
-  name       = "${var.node_pool_name}"
+resource "google_container_node_pool" "orderers" {
+  name       = "orderers"
   zone       = "${var.zone}"
   cluster    = "${google_container_cluster.primary.name}"
   node_count = "${var.node_count}"
 
   node_config {
-    preemptible  = "${var.node_config_preemptible}"
-    machine_type = "${var.node_config_machine_type}"
-
     oauth_scopes = [
-      "compute-rw",
-      "storage-ro",
-      "logging-write",
-      "monitoring",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
+
+    labels {
+      service = "ordering"
+    }
+
+    tags = "${var.tags}"
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
   }
 }
