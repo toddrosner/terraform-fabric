b486c6a503ddd2d297fc28956665bf89bce22c9a
Todd Rosner <toddrosner@gmail.com>
2018-10-15 12:02:37 -0700

    update to paths and ordering

--- manifests/deploy-resources.sh
+++ manifests/deploy-resources.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-kubectl create -f org1/namespace.yaml --save-config
+# create namespaces
+kubectl create -f namespace.yaml --save-config
+
+# create endorsing and ordering for org1
 kubectl create -f org1/endorsing.yaml --save-config
 kubectl create -f org1/ordering.yaml --save-config
-kubectl create -f org1/kafka.yaml --save-config
+
+# create kafka zookeeper
+kubectl create -f kafka.yaml --save-config
