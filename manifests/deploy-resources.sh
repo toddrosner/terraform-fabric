27dc05e39e97d6cae4a56e76aedd97f8b2c85434
Todd Rosner <toddrosner@gmail.com>
2018-10-17 13:56:18 -0700

    added several lines of logic for deploying k8s resources

--- manifests/deploy-resources.sh
+++ manifests/deploy-resources.sh
@@ -1,34 +1,85 @@
 #!/usr/bin/env bash
 
+#set -x
+
 # create namespaces
 if [ -f namespace.yaml ]; then
+  echo "Creating namespaces..."
   kubectl create -f namespace.yaml --save-config
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
 
 # create nfs server
 if [ -f nfs-server.yaml ]; then
+  echo "Creating NFS server..."
   kubectl create -f nfs-server.yaml --save-config
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
 
-# persistent volume
+# create persistent volume to nfs server
 if [ -f pv.yaml ]; then
-  kubectl create -f pv.yaml --save-config
+  echo "Creating persistent volume..."
+  # get cluster ip address of nfs server service
+  os=$(uname)
+  cluster_ip=$(kubectl get service/nfs-server -o yaml | grep clusterIP | awk '{print $2}')
+  if [[ "${os}" == "Darwin" ]]; then
+    sed -i '' "s|server:|server: ${cluster_ip}|g" pv.yaml
+    kubectl create -f pv.yaml --save-config
+    sed -i '' "s|server: ${cluster_ip}|server:|g" pv.yaml
+  else
+    sed -i "s|server:|server: ${cluster_ip}|g" pv.yaml
+    kubectl create -f pv.yaml --save-config
+    sed -i "s|server: ${cluster_ip}|server:|g" pv.yaml
+  fi
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
 
-# create endorsing for org1
+# create org1 resources
 if [ -f org1/pvc.yaml ] && [ -f org1/endorsing.yaml ] && [ -f org1/ca.yaml ] && [ -f org1/tools.yaml ]; then
+  echo "Creating org1 resources..."
   kubectl create -f org1/pvc.yaml --save-config
   kubectl create -f org1/endorsing.yaml --save-config
   kubectl create -f org1/ca.yaml --save-config
   kubectl create -f org1/tools.yaml --save-config
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
 
-# create ordering for org1-orderer
+: '
+# create org1-orderer resources
 if [ -f org1-orderer/ordering.yaml ]; then
+  echo "Creating org1-orderer resources..."
   kubectl create -f org1-orderer/ordering.yaml --save-config
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
 
-# create kafka & zookeeper
+# create kafka resources
 if [ -f kafka.yaml ]; then
+  echo "Creating Kafka resources..."
   kubectl create -f kafka.yaml --save-config
+  echo ""
+  sleep 1
+else
+  echo "YAML manifest does not exist"
+  exit 1
 fi
+'
