3a2052d3833417114f01ccb261f73d3517787e81
Todd Rosner <toddrosner@gmail.com>
2018-10-19 11:25:04 -0700

    directory naming updates

--- deployments/development/us-west1/compute/kubernetes-engine/clusters/fabric/config.tf
+++ deployments/development/us-west1/compute/kubernetes-engine/clusters/fabric/config.tf
@@ -2,11 +2,17 @@ terraform {
   backend "gcs" {
     credentials = "~/.gcp/terraform-fabric.json"
     bucket      = "terraform-fabric-tf-state"
-    prefix      = "development/us-west1/compute/compute-engine/nfs-disk"
+    prefix      = "development/us-west1/compute/kubernetes-engine/clusters/fabric"
   }
 }
 
 provider "google" {
+  credentials = "${file("~/.gcp/terraform-fabric.json")}"
+  project     = "${var.project}"
+  region      = "${var.region}"
+}
+
+provider "google-beta" {
   credentials = "${file("~/.gcp/terraform-fabric.json")}"
   project     = "${var.project}"
   region      = "${var.region}"
--- deployments/development/us-west1/networking/vpc-network/vpc-networks/terraform-fabric/config.tf
+++ deployments/development/us-west1/networking/vpc-network/vpc-networks/terraform-fabric/config.tf
@@ -2,17 +2,11 @@ terraform {
   backend "gcs" {
     credentials = "~/.gcp/terraform-fabric.json"
     bucket      = "terraform-fabric-tf-state"
-    prefix      = "development/us-west1/compute/kubernetes-engine/fabric-cluster"
+    prefix      = "development/us-west1/networking/vpc-network/vpc-networks/terraform-fabric"
   }
 }
 
 provider "google" {
-  credentials = "${file("~/.gcp/terraform-fabric.json")}"
-  project     = "${var.project}"
-  region      = "${var.region}"
-}
-
-provider "google-beta" {
   credentials = "${file("~/.gcp/terraform-fabric.json")}"
   project     = "${var.project}"
   region      = "${var.region}"
